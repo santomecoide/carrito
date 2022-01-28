bf4eef5ec992caeab2deca6b3ca1f32f5630c581
Santiago <smecoide@gmail.com>
2022-01-28 01:06:24 -0500

    add firebase prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
-  firebase: {
-    projectId: 'carrito-dccba',
-    appId: '1:664655234391:web:bb0ae5dbee9c648f71aa12',
-    storageBucket: 'carrito-dccba.appspot.com',
-    apiKey: 'AIzaSyB5akzceb3-5YbPUOOimOfI-glP7b2rN7Y',
-    authDomain: 'carrito-dccba.firebaseapp.com',
-    messagingSenderId: '664655234391',
-  },
-  production: true
+  production: true,
+  
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSyB5akzceb3-5YbPUOOimOfI-glP7b2rN7Y",
+    authDomain: "carrito-dccba.firebaseapp.com",
+    projectId: "carrito-dccba",
+    storageBucket: "carrito-dccba.appspot.com",
+    messagingSenderId: "664655234391",
+    appId: "1:664655234391:web:bb0ae5dbee9c648f71aa12"
+  }
 };
